0bd06d4a1c10d131937df7870c89b6addaed5b06
Tkinoshita <1bey1140@gmail.com>
2022-11-27 02:58:35 +0900

    作成・一覧画面

---
+++ app/Models/Memo.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Memo extends Model
+{
+    use HasFactory;
+
+    public const PRIORITY_LOW = 1;
+    public const PRIORITY_MIDDLE = 2;
+    public const PRIORITY_HIGH = 3;
+    public const PRIORITIES = [
+        self::PRIORITY_LOW    => '★',
+        self::PRIORITY_MIDDLE => '★★',
+        self::PRIORITY_HIGH   => '★★★',
+    ];
+
+    protected $guarded = ['id'];
+
+    protected $dates = [
+        'deadline'
+    ];
+}
